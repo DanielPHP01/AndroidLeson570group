3f7208d45d5ccd08f26d40bd8f35ebdd68ba37c8
Daniel <97878253+DanielPHP01@users.noreply.github.com>
2024-07-20 20:56:44 +0600

    tuda suda

--- app/src/main/java/com/example/androidleson570group/MainActivity.kt
+++ app/src/main/java/com/example/androidleson570group/MainActivity.kt
@@ -16,9 +16,6 @@ class MainActivity : AppCompatActivity() {
 
         binding.btn1.setOnClickWithShowToast(this, binding.btn1.text.toString())
         binding.btn2.setOnClickWithShowToast(this, binding.btn2.text.toString())
-        binding.btn3.setOnClickWithShowToast(this, binding.btn3.text.toString())
-        binding.btn4.setOnClickWithShowToast(this, binding.btn4.text.toString())
-        binding.btn5.setOnClickWithShowToast(this, binding.btn5.text.toString())
     }
 }
 
